63930ac893b75203e39131b731c548e252091ca7
AnkaSuska <100162260+AnkaSuska@users.noreply.github.com>
2022-03-14 19:18:32 +0000

    add file2 text

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String [] args) {
 
-            Account user2 = new Account();
-            user2.openAccount();
-            user2.showAccount();
+         Scanner scanner = new Scanner(System.in);
+
+
+         System.out.println("Ilu uzytkownikow chcesz dodac?");
+        int numberOfNewAccounts = scanner.nextInt();
+    Account tableOfNewAccounts[] = new Account[numberOfNewAccounts];
+
+    for(int i= 0; i<tableOfNewAccounts.length; i++){
+
+        tableOfNewAccounts[i]=new Account();
+        tableOfNewAccounts[i].openAccount();
+
+    }
 
     }
 
